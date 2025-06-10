7240fae781505a047ee8e9b6fd1ea53ce31ec556
Ignacio Logiudice <ignacio.logiudice@gmail.com>
2025-06-10 10:23:47 -0300

    refact(api): enrobust code

--- api/src/controllers/product.controller.ts
+++ api/src/controllers/product.controller.ts
@@ -9,25 +9,58 @@ const qas: QuestionAnswer[] = questionanswerData as QuestionAnswer[];
 const reviews: Review[] = reviewsData as Review[];
 
 export const getProductById = (req: Request, res: Response) => {
-  const product = products.find(
-    (x) => x.id === req.params.id && x.status === "active"
-  );
-  if (!product) res.status(404).json({ error: "Producto no encontrado" });
-  else res.status(200).json(product);
+  if (!req.params.id) {
+    res.status(400).json({ error: "ID de producto no proporcionado" });
+    return
+  }
+  
+  const product = products.find((x) => x.id === req.params.id);
+
+  if (!product) {
+    res.status(404).json({ error: "Producto no encontrado" });
+    return 
+  }
+  
+  if (product.status !== "active") {
+    res.status(400).json({ error: "Producto no disponible" });
+    return 
+  }
+
+  res.status(200).json(product);
 };
 
 export const getQAsByProductId = (req: Request, res: Response) => {
+  if (!req.params.id) {
+    res.status(400).json({ error: "ID de producto no proporcionado" });
+    return
+  }
+  
   const qasFiltered = qas.filter(
     (x: QuestionAnswer) => x.productId === req.params.id
   );
+
+  if (!qasFiltered || qasFiltered.length < 1) {
+    res.status(404).json([]);
+    return 
+  }
 
   res.status(200).json(qasFiltered || []);
 };
 
 export const getReviewsByProductId = (req: Request, res: Response) => {
+  if (!req.params.id) {
+    res.status(400).json({ error: "ID de producto no proporcionado" });
+    return
+  }
+
   const reviewsFiltered = reviews.filter(
     (x: Review) => x.productId === req.params.id
   );
 
-  res.status(200).json(reviewsFiltered || []);
+  if (!reviewsFiltered || reviewsFiltered.length < 1) {
+    res.status(404).json([]);
+    return 
+  } 
+
+  res.status(200).json(reviewsFiltered);
 };
