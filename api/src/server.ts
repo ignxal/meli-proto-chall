a049a68270bfcef7fb4e5bf7f97e44f7fff6c040
Ignacio Logiudice <ignacio.logiudice@gmail.com>
2025-06-10 11:49:02 -0300

    feat(api): added access log

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,10 +2,23 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import productRoutes from "./routes/product.routes";
+import fs from "node:fs";
+import path from "node:path";
 require('dotenv').config()
 
 const app = express();
+const logsDirectory = path.join(__dirname, "logs");
+const logFilePath = path.join(logsDirectory, "access.log");
+
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory, { recursive: true });
+  console.log(`Created logs directory: ${logsDirectory}`);
+}
+
+const accessLogStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
 app.use(morgan("dev")); 
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/products", productRoutes);
