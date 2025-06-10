434f8a4a24c33b1f599f8d43390112f8f3465cd5
Ignacio Logiudice <ignacio.logiudice@gmail.com>
2025-06-10 10:45:31 -0300

    refact(api): added env file + morgan

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import productRoutes from "./routes/product.routes";
+require('dotenv').config()
 
 const app = express();
+app.use(morgan("dev")); 
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Server listining on port ${PORT}`);
 });
